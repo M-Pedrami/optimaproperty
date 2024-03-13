5f3f238cd01a2b794f1af749b816b7311aa07346
Maziar Pedrami <awildroot@gmail.com>
2024-03-13 08:25:44 +0100

    PropertyHeaderImage

--- app/properties/[...id]/page.jsx
+++ app/properties/[...id]/page.jsx
@@ -1,10 +1,16 @@
 "use client";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import { getProperty } from "@/utils/requests";
 import { useParams } from "next/navigation";
 
 const PropertyPage = async () => {
   const { id } = useParams();
   const property = await getProperty(id);
-  return <div>{property.name}</div>;
+  if (!property){
+    return <h1 className="text-3xl text-center font-bold mt-10">Property Not Found</h1>
+  }
+  return (
+    <PropertyHeaderImage image={property.images[0]}/>
+  )
 };
 export default PropertyPage;
